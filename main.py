05181aa9de2367682df8c3f5a1d04231b6526ec2
Vadim Drobinin <vadim@drobinin.com>
2013-01-23 16:46:25 +0400

    Fixed PEP8

--- main.py
+++ main.py
@@ -3,19 +3,12 @@ fin = open('input.txt', 'r').readlines()
 line = fin[0].split()
 w, h = int(line[2]), int(line[5])  # params of pic
 size = (w, h)
-img = Image.new('RGBA',size, (0, 0, 0, 0))
+img = Image.new('RGBA', size, (0, 0, 0, 0))
 draw = ImageDraw.Draw(img)
 pattern = 'O'  # pattern of background
 for y in range(h):
-	try:
-		line = str(fin[y + 1])
-	except:
-		print(line)
-	for x in range(w):
-		try:
-			if line[x] != pattern:
-				draw.point((x, y))
-		except:
-			print('FAIL')
+    line = str(fin[y + 1])
+    for x in range(w):
+        if line[x] != pattern:
+            draw.point((x, y))
 img.save('result.jpeg', 'JPEG')
-
